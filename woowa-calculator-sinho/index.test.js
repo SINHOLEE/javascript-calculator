1aca3c541bf89f1428623020565a8331c99f4be1
이신호 <dltlsgh5@naver.com>
2021-03-26 00:05:07 +0900

    took 2 hours to finish it

---
+++ woowa-calculator-sinho/index.test.js
@@ -0,0 +1,21 @@
+// console.log(calculate('1', '2', '+'));
+// console.log(calculate('1', '2', '*'));
+// console.log(calculate('1', '2', '/'));
+// console.log(calculate('1', '2', '-'));
+// console.log(calculate('1', '0', '/'));
+
+// console.log(isEqualOperator('='));
+// console.log(isEqualOperator('1'));
+
+// console.log(isAC('ac'));
+// console.log(isAC('1'));
+
+// console.log(isNumber('1'));
+// console.log(isNumber('ac'));
+// console.log(isNumber('='));
+
+// console.log(isOperator('+'));
+// console.log(isOperator('-'));
+// console.log(isOperator('/'));
+// console.log(isOperator('*'));
+// console.log(isOperator('1'));
